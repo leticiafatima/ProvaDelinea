6e5dbe1cd2588321ad743a9490c4dc96005664a0
unknown <leticiafatima1887@gmail.com>
2019-11-08 11:37:28 -0300

    Prova Delinea

--- src/app/service/candidate.service.ts
+++ src/app/service/candidate.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders(
-    { 'Content-Type': 'application/json'})
+    { 'Content-Type': 'application/json' })
 };
 
 const apiUrl = 'https://delineaapi.herokuapp.com:443/candidate/';
@@ -30,6 +30,11 @@ export class CandidateService {
   getCandidateById(id: number): Observable<Candidate> {
     const url = `${apiUrl}${id}`;
     return this.http.get<Candidate>(url)
+  }
+
+  editCandidate(id, candidate): Observable<any> {
+    const url = `${apiUrl}${id}`;
+    return this.http.put(url, candidate, httpOptions)
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Candidate } from './../../model/candidate';
 import { Component, OnInit } from '@angular/core';
 import { CandidateService } from 'src/app/service/candidate.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list',
@@ -14,18 +15,63 @@ export class ListComponent implements OnInit {
   candidateId;
   candidate;
 
-  constructor(private candidateService: CandidateService) { }
+  full_name;
+  cpf;
+  rg;
+  birth_date;
+  phone;
+  username;
+  email;
+  passowrd;
+
+  constructor(private candidateService: CandidateService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
-    this.candidateService.list().subscribe( resp => {
+    this.candidateService.list().subscribe(resp => {
       this.dataSource = resp;
     })
   }
 
   getCandidate(candidate: Candidate) {
     this.candidateId = 22;
-    this.candidateService.getCandidateById(this.candidateId).subscribe( resp => {
+    this.candidateService.getCandidateById(this.candidateId).subscribe(resp => {
       this.candidate = resp;
+    })
+  }
+
+  editCandidate(
+    candidate: Candidate,
+    messageError: string = 'Erro ao Editar, por favor entre em contato com o suporte.',
+    messageSucess: string = 'Candidato editado com Sucesso!',
+    action: string = ''
+  ) {
+
+    candidate = new Candidate()
+    candidate.full_name = this.candidate.full_name;
+    candidate.cpf = this.candidate.cpf;
+    candidate.rg = this.candidate.rg;
+    candidate.birth_date = this.candidate.birth_date;
+    candidate.phone = this.candidate.phone;
+    candidate.username = this.candidate.username;
+    candidate.email = this.candidate.email;
+    candidate.password = this.candidate.password;
+
+    console.log(candidate.full_name)
+    console.log(candidate.cpf)
+
+
+    this.candidateService.editCandidate(this.candidateId, candidate).subscribe(resp => {
+      if (!resp) {
+        this.snackBar.open(messageError, action, {
+          duration: 4000,
+          panelClass: ['error-class']
+        });
+      } else {
+        this.snackBar.open(messageSucess, action, {
+          duration: 3000,
+          panelClass: ['success-class']
+        });
+      }
     })
   }
 
