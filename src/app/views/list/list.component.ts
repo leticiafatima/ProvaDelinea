265178baec36655ecd7ffe041c69b8e0dd7b0a9e
leticia fatima <leticiafatima1887@gmail.com>
2019-11-08 08:48:49 -0300

    att

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -11,24 +11,22 @@ export class ListComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'email', 'telefone', 'action'];
   dataSource: Candidate[];
-  dataCandidate
+  candidateId;
+  candidate;
 
   constructor(private candidateService: CandidateService) { }
 
   ngOnInit() {
     this.candidateService.list().subscribe( resp => {
       this.dataSource = resp;
-      this.dataCandidate = resp
-      console.log(resp)
     })
   }
 
   getCandidate(candidate: Candidate) {
-    console.log(candidate)
-    console.log(this.dataCandidate)
-    /* this.candidateService.getCandidateById(candidate['id']).subscribe( resp => {
-      this.dataCandidate = resp;
-    }) */
+    this.candidateId = 22;
+    this.candidateService.getCandidateById(this.candidateId).subscribe( resp => {
+      this.candidate = resp;
+    })
   }
 
 }
